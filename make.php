ea72f26d29ce8e6d4a0bfeccb79d2018a368f377
Avery Brooks <averyjamesbrooks@gmail.com>
2018-02-08 19:10:14 -0500

    working

--- make.php
+++ make.php
@@ -1,7 +1,38 @@
 <?php
 
-$inputFile = "1080p_with_timecode.mov";
-$maxDuration = trim(`ffmpeg -i $inputFile 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,//`);
+$disallowed = array(".DS_Store","..",".",".gitkeep");
+$sourceDir = getcwd() . DIRECTORY_SEPARATOR . "source" . DIRECTORY_SEPARATOR;
+$outputDir = getcwd() . DIRECTORY_SEPARATOR . "output" . DIRECTORY_SEPARATOR;
+
+if (!is_writable($outputDir))
+	throw new \Exception("Can't write to $outputDir");
+
+$token = date("YMDhis");
+mkdir($outputDir . $token);
+
+$outputDir = $outputDir . $token . DIRECTORY_SEPARATOR;
+
+$dir = scandir($sourceDir);
+$sources = array();
+
+foreach($dir as &$file) {
+	if(stristr($file,'.php') || in_array($file,$disallowed) || is_dir($file)) {
+		continue;
+	}
+	$sources[] = $file;
+}
+
+$inputFile = $sourceDir . $sources[0];
+$inputFileEscaped = escapeshellarg($inputFile);
+
+if (!is_file($inputFile))
+	throw new Exception("I don't know WTF to do with {$inputFile}");
+
+$cmd = "ffmpeg -i $inputFileEscaped 2>&1 | grep \"Duration\" | cut -d ' ' -f 4 | sed s/,//";
+
+echo "I want to run:\n{$cmd}\n";
+
+$maxDuration = trim(`$cmd`);
 
 $sTotal = 0;
 
@@ -23,8 +54,8 @@ echo "Duration $endMax\n";
 for($i = 0; $i < 100; $i++) {
 	$start = rand(0, $endMax);
 	echo "Getting $start\n";
-	$outputFile = "out" . $start . "-" . ($start + 2) . ".mp4";
+	$outputFile = $outputDir . "out" . $start . "-" . ($start + 2) . ".mp4";
 
 	if (!is_file($outputFile))
-		`ffmpeg -ss $start -t 2 -i $inputFile $outputFile`;
+		`ffmpeg -ss $start -t 2 -i $inputFileEscaped $outputFile`;
 }
